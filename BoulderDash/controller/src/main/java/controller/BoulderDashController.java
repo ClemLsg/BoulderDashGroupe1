4731cd48e7ee583ef492d6ca81e4bad2e2d63c2f
Antounain <antonin.paul@viacesi.fr>
2017-06-16 15:58:15 +0200

    Creating java files in the controller
    Creating all class and interfaces

---
+++ BoulderDash/controller/src/main/java/controller/BoulderDashController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import view.IView;
+
+public class BoulderDashController implements IOrderPerformer{
+
+	public BoulderDashController(IBoulderDashModel boulderDashModel){
+		
+	}
+	
+	@Override
+	public void orderPerform(UserOrder userOrder) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void play(){
+		
+	}
+	
+	public void setView(IView view){
+		
+	}
+	
+	public void tick(){
+		
+	}
+	
+}
---
+++ BoulderDash/controller/src/main/java/controller/Order.java
@@ -0,0 +1,12 @@
+package controller;
+
+public enum Order {
+	
+	UP,
+	DOWN,
+	RIGHT,
+	LEFT,
+	A,
+	B;
+	
+}
