1430caa4ca24311ea15fbb99800843b30dbd5f65
Darkdady <clement.lesage@viacesi.fr>
2017-06-19 11:00:12 +0200

    Added BoulderDashModel.java

---
+++ BoulderDash/model/src/main/java/model/BoulderDashModel.java
@@ -0,0 +1,44 @@
+package model;
+
+import model.entity.Alive;
+import model.entity.Block;
+import model.tile.Tile;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by Lesage Clément on 19/06/2017.
+ */
+
+public class BoulderDashModel {
+
+    public BoulderDashModel() {
+
+    }
+
+    public void addBlock(Block block){
+
+    }
+
+    public void removeBlock(Block block){
+
+    }
+
+    public void addAlive(Alive alive){
+
+    }
+
+    public void removeAlive(Alive alive){
+
+    }
+
+    public void addTile(Tile tile){
+
+    }
+
+    public void removeTile(Tile tile){
+
+    }
+    
+}
