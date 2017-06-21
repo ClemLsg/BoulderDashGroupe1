b484c052f196cb86eec4b6cd773e7bb685243094
Stovent <StoventTAS@gmail.com>
2017-06-21 16:59:27 +0200

    Added javadoc for MapDAOTest

--- BoulderDash/model/src/test/java/model/dao/MapDAOTest.java
+++ BoulderDash/model/src/test/java/model/dao/MapDAOTest.java
@@ -24,6 +24,10 @@ public class MapDAOTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * Instantiate a new MapDAO for the test.
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		this.id = 1;
@@ -34,6 +38,10 @@ public class MapDAOTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * tests if the id is >=1 and tests the other methods.
+	 * @throws SQLException
+	 */
 	@Test
 	public void testCreateMap() throws SQLException {
 		assert id >= 1 : "Error: id can't be < 1";
@@ -42,17 +50,29 @@ public class MapDAOTest {
 		testGetMapCode();
 	}
 
+	/**
+	 * checks that the width isn't negative.
+	 * @throws SQLException
+	 */
 	@Test
 	public void testGetMapXsize() throws SQLException {
 		System.out.println(MapDAO.getMapXsize(this.id));
 		assert MapDAO.getMapXsize(this.id) >= 0 : "Error, width can't be < 0";
 	}
 
+	/**
+	 * checks that the height isn't negative.
+	 * @throws SQLException
+	 */
 	@Test
 	public void testGetMapYsize() throws SQLException {
 		assert MapDAO.getMapYsize(id) >= 0 : "Error, height can't be < 0";
 	}
 
+	/**
+	 * checks that the mapCode has a correct length.
+	 * @throws SQLException
+	 */
 	@Test
 	public void testGetMapCode() throws SQLException {
 		assert (this.mapDAO.getMapCode(id).length() == this.mapDAO.getMapXsize(id) * this.mapDAO.getMapYsize(id) + this.mapDAO.getMapYsize(id)) || (this.mapDAO.getMapCode(id).length() == this.mapDAO.getMapYsize(id) * this.mapDAO.getMapXsize(id) + this.mapDAO.getMapYsize(id) + 1): "Error: incorrect mapCode";
