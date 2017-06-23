36839a2ed25cf554f558e4303089d23775c50f1c
Stovent <Stovent@users.noreply.github.com>
2017-06-23 16:22:04 +0200

    added SQL script

---
+++ Deliverables/DataBase/create table Maps.sql
@@ -0,0 +1,7 @@
+CREATE TABLE maps(
+        id		int (11) Auto_increment  NOT NULL ,
+        mapCode Varchar (5000) ,
+        width   Int ,
+        height  Int ,
+        PRIMARY KEY (id )
+)ENGINE=InnoDB;
