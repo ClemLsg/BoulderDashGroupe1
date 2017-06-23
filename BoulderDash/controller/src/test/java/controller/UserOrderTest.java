683aeb0b9e6044df9e7b034d4d955a6dabab1f6e
Stovent <StoventTAS@gmail.com>
2017-06-23 13:00:31 +0200

    added javadoc for UserOrderTest

--- BoulderDash/controller/src/test/java/controller/UserOrderTest.java
+++ BoulderDash/controller/src/test/java/controller/UserOrderTest.java
@@ -9,10 +9,16 @@ import org.junit.Test;
 import controller.Order;
 
 public class UserOrderTest {
+	/** the order to test */
 	Order order = Order.DOWN;
 	
+	/** the object for the tests */
 	UserOrder userOrder;
 
+	/**
+	 * instantiate a new UserOrder for the tests
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		userOrder = new UserOrder(order);
@@ -22,6 +28,9 @@ public class UserOrderTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * tests if the order is correctly set
+	 */
 	@Test
 	public void testGetOrder() {
 		Order expected = order;
